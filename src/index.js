52ae050d52c54e2f13474ffe15edfcb1f9841046
Flowcheer <lolxdwhy1@outlook.com>
2026-03-20 20:56:00 -0500

    initial weather test

--- src/index.js
+++ src/index.js
@@ -14,4 +14,5 @@ async function getWeather(latitude, longitude){
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a7b342a28a09f55016f54008735591e3`
     const res = await fetch(url)
     const data = await res.json()
+    console.log(data)
 }
